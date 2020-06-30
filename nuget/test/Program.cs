4b06defa435883351151d0b7b34f053934198d08
sergeio <sergeio@microsoft.com>
2020-06-29 17:56:01 -0700

    added initial version of the nuget package source

---
+++ nuget/test/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using BlingFire;
+
+namespace BlingUtilsTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start C# test...");
+            Console.WriteLine(String.Format("Bling Fire version: {0}", BlingFireUtils.GetBlingFireTokVersion()));
+            Console.WriteLine("Test Complete");
+        }
+    }
+}
