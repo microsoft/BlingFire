e55f5e25086c5fe749b87a8dafe347d72d7e729b
Sergei Alonichau <sergeio@microsoft.com>
2021-01-22 13:46:16 -0800

    updated helper scripts

--- scripts/normalize_corpus.py
+++ scripts/normalize_corpus.py
@@ -8,6 +8,7 @@ sys.stdin.reconfigure(encoding='utf-8')
 parser = argparse.ArgumentParser()
 parser.add_argument("-k", "--nfkc", default=False, help="enables external NFKC normalization")
 parser.add_argument("-c", "--nfc", default=False, help="enables external NFC normalization")
+parser.add_argument("-i", "--ignore-case", default=False, help="Lower-cases the intput")
 args = parser.parse_args()
 
 
@@ -20,6 +21,9 @@ for line in sys.stdin:
     # see if external normalization is needed
     if args.nfkc:
         line = unicodedata.normalize('NFKC', line)
+
+    if args.ignore_case:
+        line = line.lower()
 
     # remove leading / trailing spaces
     line = line.strip()
--- scripts/test_bling.py
+++ scripts/test_bling.py
@@ -6,19 +6,34 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-m", "--model", default="./bert_base_tok.bin", help="bin file with compiled tokenization model")
 parser.add_argument("-s", "--no-output", default=False, help="No output, False by default")
 parser.add_argument("-n", "--no-process", default=False, help="No process, False by default")
+parser.add_argument("-i", "--print-input", default=False, help="Prints input string")
+parser.add_argument("-p", "--no-padding", default=False, help="Does not pad to the max length")
+parser.add_argument("-u", "--unk", default=100, help="Unknown token id")
+parser.add_argument("-x", "--max-length", default=128, help="Maximum number of ids to return")
+parser.add_argument("-a", "--single-space", default=False, help="Output ids single as a single space delimited string")
+
 args = parser.parse_args()
 
 h = load_model(args.model)
+
+max_length = int(args.max_length)
+unk = int(args.unk)
+no_padding = args.no_padding
 
 for line in sys.stdin:
 
     line = line.strip()
 
+    if args.print_input:
+        print(line)
+
     if not args.no_process:
-        ids = text_to_ids(h, line, 128, 100)
+        ids = text_to_ids(h, line, max_length, unk, no_padding)
 
         if not args.no_output:
-            print(line)
-            print(ids)
+            if args.single_space:
+                print(" ".join([str(i) for i in ids]));
+            else:
+                print(ids)
 
 free_model(h)
